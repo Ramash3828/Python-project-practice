86851c81dcf32a7ec1709e709c2ce4f9a847d3fc
Ramash3828 <ramash3828@gmail.com>
2022-06-27 12:09:40 +0600

    implement the variables

---
+++ variable.py
@@ -0,0 +1,12 @@
+name = "Ramash"
+name2 = 'Ramash'
+
+# print(name, name2)
+# variable means as like a container. It can store different kind of data.
+
+var_String = "Hello World"  # string
+var_integer = 99  # integer
+var_float = 99.50  # float
+var_boolean = True  # bool
+var_boolean2 = False  # bool
+print(var_String, var_integer, var_float, var_boolean, var_boolean2)
